e7d293cb449d9042eac33ed544ff5848610f8238
Hui Lin <longqiman@gmail.com>
2016-10-19 16:37:52 -0500

    add function

---
+++ R/cv-glasso.R
@@ -0,0 +1,87 @@
+#' Cross-validation for glmnet 
+#' @description Does k-fold cross-validation for group lasso logistic regression and returns a list object
+#' @param trainx  a data frame where samples are in rows and features are in columns  
+#' @param trainy a numeric or factor vector containing the outcome for each sample 
+#' @param nlam    number of lambda values. The default is 100
+#' @param type  the type of prediction. type = 'link' is on the scale of linear predictors (default), whereas type = 'response' is on the scale of the response  variable, i.e. type = 'response' applies the inverse link function to the linear predictors.  
+#' @param na_action function determining what should be done with missing values when predicting new data during cross validation. The default is to predict NA.
+#' @param kfold number of folds - default is 10. Although nfolds can be as large as the sample size (leave-one-out CV), it is not recommended for large datasets. The default is 10.
+#' @return  
+#' an object of class "cv.glasso" is returned, which is a list with the ingredients of the cross-validation fit.
+#' @author 
+#' @examples 
+#' \dontrun{
+#' dat <- read.csv("/Users/happyrabbit/Documents/GitHub/DataScientistR/Data/sim1_da1.csv")
+#' trainx = dplyr::select(dat, -y)
+#' trainy = dat$y
+#' # index of the group
+#' index <- gsub("\\..*", "", names(trainx))
+#' # nlam is the number of values of tuning variable
+#' nlam <- 10
+#' # type of prediction
+#' type = "link"
+#' # number of cross-validation folds
+#' kfold <- 10
+#' cv.fit <- cv.glasso(trainx, trainy, nlam = nlam, kfold = kfold)
+#' str(cv.fit)
+#' }
+
+cv.glasso <- function(trainx, trainy, nlam = 100, type = "link", kfold = 10, 
+                      na_action = na.pass) {
+  
+  library(grplasso)
+  library(caret)
+  n <- nrow(trainx)
+  # Do grouplasso logistic regression
+  x <- cbind(1, as.matrix(trainx))
+  colnames(x) <- c("Intercept", colnames(trainx))
+  index <- c(NA, as.factor(index))
+  ################################# Get a vector of tuning parameters
+  lambda <- lambdamax(x, y = trainy, index = index, penscale = sqrt, model = LogReg()) * 
+    0.96^(1:nlam)
+  
+  index0 <- c(1:n)
+  y0 <- NULL
+  pre <- NULL
+  
+  for (i in 1:kfold) {
+    idx <- sample(index0, round(n/kfold, 0), replace = F)
+    test <- trainx[idx, ]
+    train <- trainx[-idx, ]
+    y0 <- c(y0, trainy[idx])
+    xtest <- cbind(1, as.matrix(test))
+    xtrain <- cbind(1, as.matrix(train))
+    colnames(xtrain) <- c("Intercept", colnames(train))
+    colnames(xtest) <- c("Intercept", colnames(test))
+    fit <- grplasso(xtrain, trainy[-idx], index = index, lambda = lambda, 
+                    model = LogReg(), penscale = sqrt)
+    pre0 <- predict(fit, xtest, type = type, na.action = na_action)
+    pre0 <- data.frame(pre0)
+    pre <- rbind(pre, pre0)
+    index0 <- index0[-idx]
+  }
+  
+  auc <- rep(0, ncol(pre))
+  for (i in 1:ncol(pre)) {
+    auc[i] <- as.numeric(auc(y0, pre[, i]))
+  }
+  ############################## Maximize log-likelihood
+  loglike <- rep(0, ncol(pre))
+  for (i in 1:ncol(pre)) {
+    loglike[i] <- sum((pre[, i] - log(1 + exp(pre[, i]))) * y0 + (-log(1 + 
+                                                                         exp(pre[, i]))) * (1 - y0))
+  }
+  ############################## Maximize correlation
+  co <- pre
+  maxco <- rep(0, ncol(pre))
+  for (i in 1:ncol(pre)) {
+    s <- sort(pre[, i])
+    for (j in 1:nrow(pre)) {
+      yhat <- as.numeric(pre[, i] >= s[j])
+      co[j, i] <- cor(yhat, y0)
+    }
+    maxco[i] <- max(na.omit(co[, i]))
+  }
+  res <- list(lambda=lambda, pred = pre, auc = auc, log_likelihood = loglike, maxrho = maxco)
+  return(res)
+}
